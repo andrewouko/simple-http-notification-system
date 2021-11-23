3863d7fb6876be9979da5a2ca6e20f8a98d57637
Andrew Ouko <andrew.ouko.254@gmail.com>
2021-11-23 07:48:47 +0300

    Initial commit

--- routes.js
+++ routes.js
@@ -15,34 +15,4 @@ module.exports = app => {
 
     // publish message to topic 
     app.route('/publish/:topic').post(publishMessage)
-
-
-   /*  const {listFrom, listSongs, AddToCart, filterBy, listCart, PurchaseCart, removeCartItem} = require('./controllers/catalog.js')
-    // genres
-    app.route('/genres').get(listFrom('genre'))
-    app.route('/genres/:name').get(validateList('name'), listFrom('genre'))
-
-    // artists
-    app.route('/artists').get(listFrom('artist'))
-    app.route('/artists/:name').get(validateList('name'), listFrom('artist'))
-    app.route('/artists/:genre/genre').get(validateFilter('artist', 'genre'), filterBy('artist', 'genre'))
-
-    // albums
-    app.route('/albums').get(listFrom('album'))
-    app.route('/albums/:name').get(validateList('name'), listFrom('album'))
-    app.route('/albums/:genre/genre').get(validateList('genre'), listFrom('album', 'genre'))
-    app.route('/albums/:artist/artist').get(validateList('artist'), listFrom('album', 'artist'))
-
-    // songs
-    app.route('/songs').get(listSongs())
-    app.route('/songs/:name').get(validateList('name', 'song.name'), listSongs())
-    app.route('/songs/:genre/genre').get(validateList('genre', 'genre.name'), listSongs('genre', 'genre.name'))
-    app.route('/songs/:album/album').get(validateList('album', 'album.name'), listSongs('album', 'album.name'))
-
-    // cart
-    app.route('/cart')
-        .post(validateAddToCart, AddToCart)
-        .get(listCart())
-        .put(PurchaseCart)
-        .delete(validateRemoveItemFromCart, removeCartItem) */
 };
